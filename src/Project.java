0884bbea035b7b570ce58030d36bc2a1face867c
Ginnyelf <ginnyelf0801@gmail.com>
2023-03-08 14:29:42 +0300

    Initial commit

---
+++ src/Project.java
@@ -0,0 +1,9 @@
+public class Project {
+    public static void main(String[] args) {
+        int cost_ticket = 13676;
+        int bonus_mile = 20;
+        int bonus = cost_ticket/bonus_mile;
+        System.out.println("Количество бонусных миль = " + bonus);
+
+    }
+}
